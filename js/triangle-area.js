a06a250a72b4d3b6e7c021b0ec123d4131be7256
Livien Yin <LivienYin@gmail.com>
2013-04-24 22:29:10 -0700

    used prototype to add method to object constructed with triangle constructor

--- js/triangle-area.js
+++ js/triangle-area.js
@@ -4,12 +4,13 @@ function triangle(sideA, sideB, sideC) {
     this.sideA = sideA;
     this.sideB = sideB;
     this.sideC = sideC;
-    this.area = function () {
-        var s = ((this.sideA + this.sideB + this.sideC) / 2);
-        var trianglearea = Math.sqrt(s * (s - this.sideA) * (s - this.sideB) * (s - this.sideC));
-        return trianglearea;
-    };
 }
+
+triangle.prototype.area = function () {
+    var s = ((this.sideA + this.sideB + this.sideC) / 2);
+    var trianglearea = Math.sqrt(s * (s - this.sideA) * (s - this.sideB) * (s - this.sideC));
+    return trianglearea;
+};
 
 var mytriangle = new triangle(3, 4, 5);
 alert("Triangle area is " + mytriangle.area());
